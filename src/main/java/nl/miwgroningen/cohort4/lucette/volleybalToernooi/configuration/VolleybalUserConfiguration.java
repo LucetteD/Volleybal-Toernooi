6520869ed93a1141e1aa3249776aedabd07e1973
Lucette Das <l.k.das@st.hanze.nl>
2020-12-11 11:52:18 +0100

    Messing with security configuration

--- src/main/java/nl/miwgroningen/cohort4/lucette/volleybalToernooi/configuration/VolleybalUserConfiguration.java
+++ src/main/java/nl/miwgroningen/cohort4/lucette/volleybalToernooi/configuration/VolleybalUserConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,8 +26,18 @@ public class VolleybalUserConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("admin").password(passwordEncoder().encode("admin")).roles("USER", "ADMIN");
+                .withUser("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("USER", "ADMIN");
         auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/teams*").permitAll()
+                .antMatchers("/resources/**", "/static/**","/webjars/**").permitAll()
+                .anyRequest().authenticated();
     }
 
     @Bean
